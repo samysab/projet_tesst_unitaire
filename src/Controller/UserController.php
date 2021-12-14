0669ea16dc2f6a9dadf58a5c80a6f205e087cf6a
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2021-12-14 10:36:43 +0100

    Push vendor

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Carbon\Carbon;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +16,22 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    public function isValid(){
+        if ($this->email != null && filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+            if ($this->nom != null && $this->prenom != null){
+
+                $carbon = new Carbon($this->dateNaissance, new DateTimeZone('Europe/Paris'));
+
+                if (Carbon::parse($carbon)->age > 12) {
+                    return true;
+                }else{
+                    throw new Exception("trop petit");
+                }
+            }
+        }else{
+            throw new Exception("arg manquant");
+        }
     }
 }
